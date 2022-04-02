15019c25622a0acc8e9353b12070bc2331eb3510
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2022-04-02 12:04:59 -0300

    :tada: initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./styles";
+
+const App: React.FC = () => {
+	return <Container />;
+};
+
+export { App };
---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled } = createStitches({
+	theme: {
+		colors: {
+			right: "#3dae9b",
+			place: "#d59a67",
+			wrong: "#2d2a32",
+			slotBorder: "#47434c",
+			slotLocked: "#58535f",
+			background: "#645d6f",
+			white: "#fcfaff",
+		},
+	},
+});
